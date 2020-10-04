0b906cc401353b2539d0fe4cd245745b224b221d
Sam Gold <sgold5757@gmail.com>
2020-10-04 17:52:59 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   for i in array do
     if i == value_to_find
-      return array(value_to_find).index
+      return array.index(value_to_find)
     end
   end
   nil
